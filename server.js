800ab8b0fa24bb5893733df60d517535a7b14f80
Emmanuel Ezele <emmaezele@yahoo.com>
2022-07-18 13:57:09 +0100

    Updated File

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-const express = require("express ")
+const express = require("express")
+const connectDB = require("./db/index")
+require("dotenv").config()
+
+//initialize express
+const app = express()
+
+//create a port
+const PORT = process.env.PORT
+
+//connect to db
+connectDB()
+
+
+//Initialize express middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//create a basic route
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "welcome to our tutor app" })
+})
+
+//listen
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
